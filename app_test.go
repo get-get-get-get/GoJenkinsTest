34423ab7dd24a5bc5fae29cec7497d997148bd0d
Get-Get-Get-Get <51015518Get-Get-Get-Get@users.noreply.github.com>
2021-03-10 16:01:00 -0500

    add tests for SubtractNumbers

--- app_test.go
+++ app_test.go
@@ -13,7 +13,6 @@ func init() {
 
 // Test AddNumbers with correct usage
 func TestAddNumbers(t *testing.T) {
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/add/2/4", nil)
 	router.ServeHTTP(w, req)
@@ -24,12 +23,30 @@ func TestAddNumbers(t *testing.T) {
 
 // Test AddNumbers where param cannot be converted to int
 func TestAddNumbersBadRequest(t *testing.T) {
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/add/2/four", nil)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 	assert.Equal(t, addNumbersUsageMsg, w.Body.String())
+}
 
+// Test SubtractNumbers with correct usage
+func TestSubtractNumbers(t *testing.T) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/subtract/2/4", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "-4", w.Body.String())
+}
+
+// Test SubtractNumbers where param cannot be converted to int
+func TestSubtractNumbersBadRequest(t *testing.T) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/subtract/2/four", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, subtractNumbersUsageMsg, w.Body.String())
 }
